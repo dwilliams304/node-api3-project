17ca5a400125bdc801d3ea56078e1c058c8730cb
dwilliams304 <d.williams0434@gmail.com>
2023-09-24 15:12:38 -0700

    Middleware, get all users, get user

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const { logger, validatePost, validateUser, validateUserId } = require('./middleware/middleware')
+
+const userRouter = require('./users/users-router');
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+//Routers
+server.use('/api/users', userRouter);
+
+//Middlewares
+server.use(logger);
+server.use(validatePost);
+server.use(validateUser);
+server.use(validateUserId);
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
